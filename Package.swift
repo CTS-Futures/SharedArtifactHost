d867fd1146a62ce43c51957e3acbc704de56f3fc
Alex Mortimer <alex.mortimer@plus500.com>
2026-01-09 13:12:28 -0600

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+   name: "Shared",
+   platforms: [
+     .iOS(.v14),
+   ],
+   products: [
+      .library(name: "Shared", targets: ["Shared"])
+   ],
+   targets: [
+      .binaryTarget(
+         name: "Shared",
+         url: "https://github.com/CTS-Futures/T4Mobile/releases/download/shared-1.0.0-alpha02/Shared-1.0.0-alpha02.xcframework.zip",
+         checksum:"4f25536177e144553d572ce3c4309b967ba9012551d1866d597458123240b9e2")
+   ]
+)
