2ccc4846268eaeb7ab74fd1ad8fd5be1b3d0e6f8
Alex Mortimer <alex.mortimer@plus500.com>
2026-01-09 13:15:57 -0600

    Update Package.swift with new repo url

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "Shared",
-         url: "https://github.com/CTS-Futures/T4Mobile/releases/download/shared-1.0.0-alpha02/Shared-1.0.0-alpha02.xcframework.zip",
+         url: "https://github.com/CTS-Futures/SharedArtifactHost/releases/download/v1.0.0-alpha02/Shared-1.0.0-alpha02.xcframework.zip",
          checksum:"4f25536177e144553d572ce3c4309b967ba9012551d1866d597458123240b9e2")
    ]
 )
